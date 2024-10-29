070c87fdc320f253db5113882e2c540e0b818720
harry <hk.mca08@gmail.com>
2024-10-29 20:49:35 +0530

    git_action3010u47

--- ios/Classes/receiver/AnyEventHandler.swift
+++ ios/Classes/receiver/AnyEventHandler.swift
@@ -1,25 +1,25 @@
-import Flutter
-import UIKit
-import Foundation
-
-public class AnyEventHandler: NSObject, FlutterPlugin, FlutterStreamHandler {
-    private var eventSink: FlutterEventSink?
-
-    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        self.eventSink = events
-        // Example: self.eventSink?("Event data")
-        return nil
-    }
-
-    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        self.eventSink = nil
-        return nil
-    }
-
-    // Method to trigger the event from the plugin
-    public func triggerEvent(eventData: String) {
-        if let eventSink = self.eventSink {
-            eventSink(eventData) // Send event data to Flutter
-        }
-    }
-}
+//import Flutter
+//import UIKit
+//import Foundation
+//
+//public class AnyEventHandler: NSObject, FlutterPlugin, FlutterStreamHandler {
+//    private var eventSink: FlutterEventSink?
+//
+//    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+//        self.eventSink = events
+//        // Example: self.eventSink?("Event data")
+//        return nil
+//    }
+//
+//    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+//        self.eventSink = nil
+//        return nil
+//    }
+//
+//    // Method to trigger the event from the plugin
+//    public func triggerEvent(eventData: String) {
+//        if let eventSink = self.eventSink {
+//            eventSink(eventData) // Send event data to Flutter
+//        }
+//    }
+//}
